e82c8bddc616fb576648d00186f79dc2416c3afb
mathusihan <mathusihan@gmail.com>
2019-03-11 23:40:49 -0400

    This version can delete words on list by typing them

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # cps630-test-server
 
+
+NOTES /////////////////////////////////
 Have a channel for each lobby
+AngularJS does not allow for duplicate elements
 
 Adding to channel
 - Player send messages to chanel
@@ -8,18 +11,19 @@ Adding to channel
 - Players all type onto channel
 - Player sends message to channel with word they typed. JS checks if its matches with word bank then
 - Add to channels as player join. If 6 people join one channel, create new one and add users to that one now, and close old channel.
-- FOR NOW create function that calculates points of words and gives it to user.
-- 
-
 
 TESTING
 - Have <li> list and remove words from html when user types it.
-
-
-
 
 MESSAGES 
 - Player messages channel with word 
 - Callback(function) have callback match any word in wordbank if it dosn't return false, else return true
 - Listener listens to callback and infroms user if word is correct. listener will recieve true or false values.
+- Send user points in message object. At the very end save message object onto another variable and put it in database.
+- Get uuid from message and update that persons points
 -
+
+END OF GAME
+- Set time limit for game
+- Once time limit is up send "over" message to channel
+- Channel will save all results  and terminate itself
--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -1,13 +1,22 @@
+
+var val1;
+var val2;
+
 angular.module('app', ["pubnub.angular.service"])
     .controller('ChatCtrl', function($scope, Pubnub) {
         $scope.channel = 'messages-channel';
+       $scope.value=0;
         // Generating a random uuid between 1 and 100 using an utility function from the lodash library.         
         $scope.uuid = "1";
+        $scope.points = 0;
         Pubnub.init({
             publish_key: 'pub-c-1f91d296-28dd-4d8e-9e54-9ac3393a9772',
             subscribe_key: 'sub-c-de35a980-441a-11e9-bd6d-163ac0efd868',
-            uuid: $scope.uuid
+            uuid: $scope.uuid,
+          //  value: $scope.value
         });
+
+ 
 
         // Send the messages over PubNub Network
         $scope.sendMessage = function() {
@@ -15,22 +24,43 @@ angular.module('app', ["pubnub.angular.service"])
             if (!$scope.messageContent || $scope.messageContent === '') {
                 return;
             }
+
             Pubnub.publish({
                 channel: $scope.channel,
                 message: {
                     content: $scope.messageContent,
                     sender_uuid: $scope.uuid,
                     date: new Date()
+                    //points=$scope.points
                 },
                 callback: function(m) {
                     console.log(m); // callback object function fires when we finish sending a message(m) to the channel
                 }
             });
-            // Reset the messageContent input
+           
+           
+             // Reset the messageContent input
             $scope.messageContent = '';
         }
 
+
+
         $scope.messages = [];
+        $scope.words=["hello","bye","tonight","math","math"];
+        
+
+        $scope.findword = function(string) {
+            for (let i=0; i< $scope.words.length; i++) {
+                if (string===$scope.words[i]) {
+                    $scope.words.splice( $scope.words.indexOf(string), 1 );
+                    return true;  
+            }
+            }
+
+            return false;
+        }
+
+     
 
         // Subscribing to the ‘messages-channel’ and trigering the message callback
         Pubnub.subscribe({
@@ -38,15 +68,23 @@ angular.module('app', ["pubnub.angular.service"])
             triggerEvents: ['callback']
         });
 
-        // Listening to the callbacks
+
+
+        // Listening to the callbacks THIS IS WHERE THE SYNCING HAPPENS
         $scope.$on(Pubnub.getMessageEventNameFor($scope.channel), function(ngEvent, m) {
             $scope.$apply(function() {
-                $scope.messages.push(m) // push message m onto messages array
+                
+                $scope.messages.push(m); // push message m onto messages array
+                // M IS OBJECT OF SENT OBJECT {content:.., uuid:..)}
+               if ( $scope.findword(m.content)) {
+            
+
+            }
+
+                console.log(m.uuid);
+               // console.log($scope.messages);
             });
         });
 
-        // A function to display a nice uniq robot avatar 
-        $scope.avatarUrl = function(uuid) {
-            return 'http://robohash.org/' + uuid + '?set=set2&bgset=bg2&size=70x70';
-        };
+         
     });
