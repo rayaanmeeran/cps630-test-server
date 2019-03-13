7f8b30f2c19c6a17a00ee9d7c5fc13262aaba27f
Rayaan Meeran <rayaannmeeran@gmail.com>
2019-03-13 16:30:57 -0400

    format

--- chat-example-master/index.js
+++ chat-example-master/index.js
@@ -4,18 +4,18 @@ var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
+io.on('connection', function(socket) {
+    socket.on('chat message', function(msg) {
+        io.emit('chat message', msg);
+    });
 });
 
 
 
-http.listen(port, function(){
-  console.log('listening on *:' + port);
+http.listen(port, function() {
+    console.log('listening on *:' + port);
 });
