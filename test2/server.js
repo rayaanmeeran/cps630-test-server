7a393150bf4c63187b321f43ccbe97e20a3e8eff
mathusihan <mathusihan@gmail.com>
2019-03-18 18:18:31 -0400

    test

--- test2/server.js
+++ test2/server.js
@@ -9,47 +9,6 @@ var connections=[];
 var room = '';
 var team=0;
 
-function updateUsers(socket) {
-room = parseInt(connections.length/3).toString();
-
-
-for (let i=0; i<=room; i++) {
-  var clientsInRoom = io.nsps['/'].adapter.rooms[i.toString()];
-  var numClients = clientsInRoom === undefined ? 0 : Object.keys(clientsInRoom.sockets).length;
-  
-  
-  if (parseInt(numClients)<2) {
-    socket.join(i.toString());
-  
-  
-    var team1 = (team%2)+1;
-    team++;
-    socket.team=team1;
- 
-    console.log("In room " + i + " there are " + (numClients+1) + " people " +" \n Socket team is " +socket.team);
-    break;
-  }
-
-}
-
-
-//socket.join(room);
-/*
-  if (numofUsers===1) {
-    socket.join('1');
-    io.sockets.adapter.rooms['1'].var = 'fdsfds';
-    room='1';
-  }
-
-  else  {
-    socket.join('2');
-    io.sockets.adapter.rooms['2'].var = 'ldldl';
-    room='2';
-  }
-*/
-
-}
-
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
@@ -59,17 +18,11 @@ io.on('connection', function(socket){ // SOCKET.ID IS UNIQE TO EACH PERSON
    numofUsers++;
 connections.push(socket);
 
-  updateUsers(socket);
+
+updateUsers(socket);
+  
 
 //  console.log(numClients);
-
-  socket.on('disconnect', function() { 
-    numofUsers--; 
-  connections.splice(connections.indexOf(socket),1);
-//console.log("Disconnected: Num of people " + connections.length + " In room "+room+" is: "+ conInCurRoom);
-  
-  });
-
   var currentRoom=Object.keys( io.sockets.adapter.sids[socket.id])[0];
   var conInCurRoom =  io.sockets.adapter.rooms[currentRoom].length;
 
@@ -77,20 +30,19 @@ connections.push(socket);
   
 if (conInCurRoom===2) { // If tehre are two people in the room start.
   io.to(currentRoom).emit('start');
-  currentRoom.start=true;
+  io.sockets.adapter.rooms[currentRoom].start=true;
 }
 else {
   io.to(currentRoom).emit('not ready');
-  currentRoom.start=false;
+  //io.sockets.adapter.rooms[currentRoom].start=false;
 }
 
-  
+//console.log(io.sockets.adapter.rooms[currentRoom].start);  
   socket.on('chat message', function(msg){
     io.to(currentRoom).emit('chat message', msg); 
     usernames[socket.id]=msg;  
     //console.log("  room: "+ currentRoom + " words are " +  io.sockets.adapter.rooms[currentRoom].words);
   });
-
 
 
 socket.on('setArr', function(newWords) {
@@ -99,11 +51,47 @@ io.to(currentRoom).emit('sentNewArray',io.sockets.adapter.rooms[currentRoom].wor
 //console.log("  room: "+ currentRoom + " words are " +  io.sockets.adapter.rooms[currentRoom].words);
 });
 
-
+socket.on('disconnect', function() { 
+  numofUsers--; 
+  connections.splice(connections.indexOf(socket),1);
+//console.log("Disconnected: Num of people " + connections.length + " In room "+room+" is: "+ conInCurRoom);
+  });
  
-});
 
 
+  
+
+
+
+}); // END OF IO CONNETION
+
+
+
+function updateUsers(socket) {
+  room = parseInt(connections.length/3).toString();
+  //var currentRoom=Object.keys( io.sockets.adapter.sids[socket.id])[0];
+  //console/log(currentRoom.start);
+  
+  for (let i=0; i<=room; i++) {
+    var clientsInRoom = io.nsps['/'].adapter.rooms[i.toString()];
+    var numClients = clientsInRoom === undefined ? 0 : Object.keys(clientsInRoom.sockets).length; 
+    var isFull= clientsInRoom === undefined ? false : Object.keys(clientsInRoom).start;
+    console.log(isFull);
+
+    if (parseInt(numClients)<2 && !isFull) {
+     // var x = io.sockets.adapter.rooms['0'].start;
+      socket.join(i.toString());
+      var team1 = (team%2)+1;
+      team++;
+      socket.team=team1;
+    //  console.log("In room " + i + " there are " + (numClients+1) + " people " +" \n Socket team is " +socket.team);
+      break;
+    }
+  
+  } // END OF FOR
+  
+  
+  } // END OF UPDATE FUNC
 
 
 http.listen(port, function() {
