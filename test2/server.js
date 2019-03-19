a0c1ff15415f68ae41978cc2f6570af16ad50eae
mathusihan <mathusihan@gmail.com>
2019-03-19 14:51:05 -0400

    point system

--- test2/server.js
+++ test2/server.js
@@ -1,152 +1,126 @@
-var express = require('express');
-var app = express();
-var http = require('http').Server(app);
+var app = require('express')();
+var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
 var numofUsers = 0;
 var usernames = {};
 var connections = [];
-var room = '';
+var numOfRooms = 0;
 var team = 0;
 
-http.listen(port, function() {
-    console.log('\x1b[36m%s\x1b[0m', "Server running..." + "\n" + "Listening on port: " + port); //First part of the console.log sets its color.
-});
-
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/index.html');
-});
-
-app.use(express.static(__dirname + "/"));
 
 function updateUsers(socket) {
-    room = parseInt(connections.length / 3).toString();
+    numOfRooms = parseInt(connections.length / 3);
+    var numClients = 0;
+    var newRoom;
 
-
-    for (let i = 0; i <= room; i++) {
+    for (let i = 0; i <= numOfRooms; i++) {
         var clientsInRoom = io.nsps['/'].adapter.rooms[i.toString()];
-        var numClients = clientsInRoom === undefined ? 0 : Object.keys(clientsInRoom.sockets).length;
+        numClients = clientsInRoom === undefined ? 0 : Object.keys(clientsInRoom.sockets).length;
+        var started = io.sockets.adapter.rooms[i.toString()];
+        var isStarted = started === undefined ? false : io.sockets.adapter.rooms[i.toString()].start;
 
 
-        if (parseInt(numClients) < 2) {
+        if (parseInt(numClients) < 2 && !isStarted) {
             socket.join(i.toString());
-
-
-
-updateUsers(socket);
-  
-
-//  console.log(numClients);
-  var currentRoom=Object.keys( io.sockets.adapter.sids[socket.id])[0];
-  var conInCurRoom =  io.sockets.adapter.rooms[currentRoom].length;
-
-//console.log("Connected: Num of people " + connections.length + " In room "+room+" is: "+ conInCurRoom);
-  
-if (conInCurRoom===2) { // If tehre are two people in the room start.
-  io.to(currentRoom).emit('start');
-  io.sockets.adapter.rooms[currentRoom].start=true;
-}
-else {
-  io.to(currentRoom).emit('not ready');
-  //io.sockets.adapter.rooms[currentRoom].start=false;
-}
-
-//console.log(io.sockets.adapter.rooms[currentRoom].start);  
-  socket.on('chat message', function(msg){
-    io.to(currentRoom).emit('chat message', msg); 
-    usernames[socket.id]=msg;  
-    //console.log("  room: "+ currentRoom + " words are " +  io.sockets.adapter.rooms[currentRoom].words);
-  });
             var team1 = (team % 2) + 1;
             team++;
             socket.team = team1;
-
-            console.log('\x1b[35m%s\x1b[0m', "In room " + i + " there are " + (numClients + 1) + " people.");
-            console.log('\x1b[32m%s\x1b[0m', "Socket team is " + socket.team);
+            newRoom = i.toString();
+            //console.log("In numOfRooms " + i + " there are " + (numClients+1) + " people " +" \n Socket team is " +socket.team);
             break;
         }
 
     }
 
+    /*if (fullRooms===numOfRooms && fullRooms<1) {
+      newRoom=(parseInt(newRoom)+1).toString(); 
+      numOfRooms++;
+      socket.join((newRoom).toString());
+      io.to(newRoom).emit('start',newRoom);
+      }*/
+    if (newRoom === undefined) {
+        newRoom = (numOfRooms + 1).toString();
+        numOfRooms++;
+        socket.join(newRoom);
+    }
 
+    clientsInRoom = io.nsps['/'].adapter.rooms[newRoom];
+    numClients = clientsInRoom === undefined ? 0 : Object.keys(clientsInRoom.sockets).length;
+
+
+    //else {
+    if (numClients === 2) { // If tehre are two people in the numOfRooms start.
+        io.to(newRoom).emit('start', newRoom);
+        io.sockets.adapter.rooms[newRoom].start = true;
+        io.sockets.adapter.rooms[newRoom].points1 = 0;
+        io.sockets.adapter.rooms[newRoom].points2 = 0;
+
+        console.log(io.sockets.adapter.rooms[newRoom].start);
+    } else {
+        io.to(newRoom).emit('not ready');
+        // io.sockets.adapter.rooms[currentRoom].start=false;
+    }
+    //} // END OF BIG ELSE
+
+} // END OF FUNC
+
+
+function wordPoint(word) {
+    return word.length * 10;
 }
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
 
 io.on('connection', function(socket) { // SOCKET.ID IS UNIQE TO EACH PERSON
     numofUsers++;
     connections.push(socket);
-
+    socket.points = 0;
     updateUsers(socket);
+    var currentRoom = Object.keys(io.sockets.adapter.sids[socket.id])[0];
+    //  console.log(numClients);
 
-socket.on('setArr', function(newWords) {
-io.sockets.adapter.rooms[currentRoom].words=newWords;
-io.to(currentRoom).emit('sentNewArray',io.sockets.adapter.rooms[currentRoom].words);
-//console.log("  room: "+ currentRoom + " words are " +  io.sockets.adapter.rooms[currentRoom].words);
-});
+    socket.on('disconnect', function() {
+        numofUsers--;
+        connections.splice(connections.indexOf(socket), 1);
+        //console.log("Disconnected: Num of people " + connections.length + " In numOfRooms "+numOfRooms+" is: "+ conInCurRoom);
 
-socket.on('disconnect', function() { 
-  numofUsers--; 
-  connections.splice(connections.indexOf(socket),1);
-//console.log("Disconnected: Num of people " + connections.length + " In room "+room+" is: "+ conInCurRoom);
-  });
- 
-
-    //console.log("Connected: Num of people " + connections.length + " In room "+room+" is: "+ conInCurRoom);
-
-  
-
-
-
-}); // END OF IO CONNETION
-
-
-
-function updateUsers(socket) {
-  room = parseInt(connections.length/3).toString();
-  //var currentRoom=Object.keys( io.sockets.adapter.sids[socket.id])[0];
-  //console/log(currentRoom.start);
-  
-  for (let i=0; i<=room; i++) {
-    var clientsInRoom = io.nsps['/'].adapter.rooms[i.toString()];
-    var numClients = clientsInRoom === undefined ? 0 : Object.keys(clientsInRoom.sockets).length; 
-    var isFull= clientsInRoom === undefined ? false : Object.keys(clientsInRoom).start;
-    console.log(isFull);
-
-    if (parseInt(numClients)<2 && !isFull) {
-     // var x = io.sockets.adapter.rooms['0'].start;
-      socket.join(i.toString());
-      var team1 = (team%2)+1;
-      team++;
-      socket.team=team1;
-    //  console.log("In room " + i + " there are " + (numClients+1) + " people " +" \n Socket team is " +socket.team);
-      break;
-    }
-  
-  } // END OF FOR
-  
-  
-  } // END OF UPDATE FUNC
-    if (conInCurRoom === 2) { // If tehre are two people in the room start.
-        io.to(currentRoom).emit('start');
-        currentRoom.start = true;
-    } else {
-        io.to(currentRoom).emit('not ready');
-        currentRoom.start = false;
-    }
+    });
 
 
     socket.on('chat message', function(msg) {
         io.to(currentRoom).emit('chat message', msg);
         usernames[socket.id] = msg;
-        //console.log("  room: "+ currentRoom + " words are " +  io.sockets.adapter.rooms[currentRoom].words);
+        //console.log("  numOfRooms: "+ currentRoom + " words are " +  io.sockets.adapter.rooms[currentRoom].words);
     });
 
 
+    socket.on('correct', function(msg) {
+        if (parseInt(msg) === 1) {
+            io.sockets.adapter.rooms[currentRoom].points1 += 1;
+        } else if (parseInt(msg) === 2) {
+            io.sockets.adapter.rooms[currentRoom].points2 += 1;
+        }
+        console.log(typeof(msg));
+        console.log("Socket on team " + socket.team + " has " + socket.points + "points\n" + " Team 1: " + io.sockets.adapter.rooms[currentRoom].points1 + " Team 2: " + io.sockets.adapter.rooms[currentRoom].points2);
+    });
 
     socket.on('setArr', function(newWords) {
         io.sockets.adapter.rooms[currentRoom].words = newWords;
         io.to(currentRoom).emit('sentNewArray', io.sockets.adapter.rooms[currentRoom].words);
-        //console.log("  room: "+ currentRoom + " words are " +  io.sockets.adapter.rooms[currentRoom].words);
+        //console.log("  numOfRooms: "+ currentRoom + " words are " +  io.sockets.adapter.rooms[currentRoom].words);
     });
 
+
+
+});
+
+
+
+
+http.listen(port, function() {
+    console.log('listening on *:' + port);
 });
